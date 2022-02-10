873540b935fcc6ac812f30b979611f5ada4d5ae0
belmeffio <marco.reggio@protonmail.com>
2022-02-10 19:11:45 +0100

    better comments

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # 24hBS-testBE
 Test for coding BE
 
+## The code
 Technology used: NodeJS, for simple API building.
 Main module: Express, mainly to help routing
 
+## The data
 The data consist in a JSON file "jokes.json" contained in "data" folder. 
 It's an array of elements of the given format:
 {
@@ -15,18 +17,24 @@ It's an array of elements of the given format:
 "updated_at": DATETIME, the last updated date time
 "categories": STRING[], the list of categories for the JOKE
 }
+This could have been done better using "json-server" module.
 
+## The routes
 Routes are set separately from the server in the /routes folder
 
+## How to install
 This is a very simple Node API, just:
 
-git clone
-npm install
-npm start (for development use npm dev)
+>git clone
+>npm install
+>npm run start 
+>(for development use npm run dev)
 
+## Testing
 To help testing it's included a JSON of a collection of calls, to import in Postman
 
-
+### Comments
 To explain the developing choices please refere to the comments in the code.
 
+# The End
 Thanks
--- routes/jokes.js
+++ routes/jokes.js
@@ -1,149 +1,173 @@
-const { param } = require('express/lib/request');
-const { getEnvironmentData } = require('worker_threads');
+const { param } = require("express/lib/request");
+const { getEnvironmentData } = require("worker_threads");
 
 // Here are defined all the routes to the server
 const jokeRoutes = (app, fs) => {
-    // variables
-    const dataPath = './data/jokes.json';
-    const querystring = require('querystring');
+  // variables
+  const dataPath = "./data/jokes.json";
+  const querystring = require("querystring");
 
-    //
-    // START utilities
-    // These are defined here to be reusable after in the code
+  //
+  // START utilities
+  // These are defined here to be reusable after in the code
 
-    //This reads from the dataPath all records
-    const readFile = (
-      callback,
-      returnJson = false,
-      filePath = dataPath,
-      encoding = 'utf8'
-    ) => {
-      fs.readFile(filePath, encoding, (err, data) => {
-        if (err) {
-          throw err;
-        }
-
-        callback(returnJson ? JSON.parse(data) : data);
-      });
-    };
-
-    //This writes on the dataPath all records
-    const writeFile = (
-      fileData,
-      callback,
-      filePath = dataPath,
-      encoding = 'utf8'
-    ) => {
-      fs.writeFile(filePath, fileData, encoding, err => {
-        if (err) {
-          throw err;
-        }
-
-        callback();
-      });
-    };
-
-    // Extremely simple fuction to search in our data array the first elements with given id
-    const findJoke = (data,jokeId) => {
-      for (const joke in data) {
-        if (data[joke].id  == jokeId) {
-          return joke;
-        }
+  //This reads from the dataPath all records
+  const readFile = (
+    callback,
+    returnJson = false,
+    filePath = dataPath,
+    encoding = "utf8"
+  ) => {
+    fs.readFile(filePath, encoding, (err, data) => {
+      if (err) {
+        throw err;
       }
-      return -1;
+
+      callback(returnJson ? JSON.parse(data) : data);
+    });
+  };
+
+  //This writes on the dataPath all records
+  const writeFile = (
+    fileData,
+    callback,
+    filePath = dataPath,
+    encoding = "utf8"
+  ) => {
+    fs.writeFile(filePath, fileData, encoding, (err) => {
+      if (err) {
+        throw err;
+      }
+
+      callback();
+    });
+  };
+
+  // Extremely simple fuction to search in our data array the first elements with given id
+  const findJoke = (data, jokeId) => {
+    for (const joke in data) {
+      if (data[joke].id == jokeId) {
+        return joke;
+      }
     }
+    return -1;
+  };
 
-    //
-    // END utilities
-  
+  //
+  // END utilities
 
-    // READ QUERY
-    /*
+  // CREATE
+  app.post("/joke", (req, res) => {
+    // Get the current data
+    readFile((data) => {
+      // TODO:
+      // Would be nice to check format of the given data, and generate automatically an UUID if not given (maybe with crypto module)
+
+      // Find the unique id given
+      const jokeId = req.body.id;
+
+      // Find the joke (if id is not given this check can be excluded)
+      const index = findJoke(data, jokeId);
+      if (index >= 0) {
+        res.status(200).send(`There is already a joke with id:${jokeId}`);
+      } else {
+        // add the new joke
+        data.push(req.body);
+        // write the new data
+        writeFile(JSON.stringify(data, null, 2), () => {
+          res.status(200).send("your joke has been added");
+        });
+      }
+    }, true);
+  });
+
+  // READ QUERY
+  /*
     form expected is:
       /search/?query={stringToSearch1},{stringToSearch2},.....
     */
-    app.get('/jokes/search/', (req, res) => {
-      // Get the query string
-      const query = req.query.query;
-      // If the query string exists we can work
-      if (typeof query !== "undefined" && query.length > 0) {
-        // Split the query on comma, so multiple strings can be searched
-        const params = query.split(',');
-        // Check if query strings are too short
-        if (params.some(param => {
+  app.get("/jokes/search/", (req, res) => {
+    // Get the query string
+    const query = req.query.query;
+    // If the query string exists we can work
+    if (typeof query !== "undefined" && query.length > 0) {
+      // Split the query on comma, so multiple strings can be searched
+      const params = query.split(",");
+      // Check if query strings are too short
+      if (
+        params.some((param) => {
           return param.length < 3;
-        })) {
-          // Exit if there are some
-          res.status(200).send(`Expected query string longer than 2 characters`);
-          res.end();
-          return;
-        }
-        // Otherwise read the data
-        readFile(data => {
-          // Find the jokes
-          // filter function is pretty but does not scale well on big dataset
-          const jokes = data.filter(
-            (joke) => {
-              // find joke with query param in value
-              for (var i = 0; i < params.length; i++) {
-                if (joke.value.toLowerCase().includes(params[i].toLowerCase())) {
-                  return true;
-                }
-              }
-              return false;
+        })
+      ) {
+        // Exit if there are some
+        res.status(200).send(`Expected query string longer than 2 characters`);
+        res.end();
+        return;
+      }
+      // Otherwise read the data
+      readFile((data) => {
+        // Find the jokes
+        // filter function is pretty but does not scale well on big dataset
+        const jokes = data.filter((joke) => {
+          // find joke with query param in value
+          for (var i = 0; i < params.length; i++) {
+            if (joke.value.toLowerCase().includes(params[i].toLowerCase())) {
+              return true;
             }
-          );
-          // Send the jokes found
-          res.send(jokes);
-        }, true);
-      }
-      // If there is no query string instruct the client 
-      else {
-        res.status(200).send(`Expected something like /search/?query={stringToSearch1},{stringToSearch2}`);
-      }
-    });
-    
-
-    // READ ALL
-    // Just returns all the jokes in the dataset
-    app.get('/jokes', (req, res) => {
-      readFile(data => {
-        res.send(data);
+          }
+          return false;
+        });
+        // Send the jokes found
+        res.send(jokes);
       }, true);
-    });
+    }
+    // If there is no query string instruct the client
+    else {
+      res
+        .status(200)
+        .send(
+          `Expected something like /search/?query={stringToSearch1},{stringToSearch2}`
+        );
+    }
+  });
 
-  
-    // READ SINGLE
-    // Note the route is different from the routes on multiple jokes (is <joke> and not <jokes>), maybe it's not too restful
-    // Gets the id as the argument after joke
-    app.get('/joke/:id', (req, res) => {
-      // read the data first
-      readFile(data => {
-        // get the id
-        const jokeId = req.params['id'];
+  // READ ALL
+  // Just returns all the jokes in the dataset
+  app.get("/jokes", (req, res) => {
+    readFile((data) => {
+      res.send(data);
+    }, true);
+  });
 
-        // Find the joke, low memory effort
-        const index = findJoke(data,jokeId);
-        if(index >= 0) {
-          res.send(data[index]);
-        }
-        else {
-          res.status(200).send(`There is no joke with id:${jokeId}`);
-        }
-      }, true);
-    });
+  // READ SINGLE
+  // Note the route is different from the routes on multiple jokes (is <joke> and not <jokes>), maybe it's not too restful
+  // Gets the id as the argument after joke
+  app.get("/joke/:id", (req, res) => {
+    // read the data first
+    readFile((data) => {
+      // get the id
+      const jokeId = req.params["id"];
 
+      // Find the joke, low memory effort
+      const index = findJoke(data, jokeId);
+      if (index >= 0) {
+        res.send(data[index]);
+      } else {
+        res.status(200).send(`There is no joke with id:${jokeId}`);
+      }
+    }, true);
+  });
 
-    // UPDATE
-    // Note the route is different from the routes on multiple jokes (is <joke> and not <jokes>), maybe it's not too restful
-    // Gets the id as the argument after joke
-    app.put('/joke/:id', (req, res) => {
-      // TODO:
-      // Would be nice to check the data given, matching it with a template format
-      const joke = req.body;
-      // Just check if present for now
-      if (typeof joke === "undefined" || joke.length < 1) {
-        res.status(200).send(`Expected a body like this: 
+  // UPDATE
+  // Note the route is different from the routes on multiple jokes (is <joke> and not <jokes>), maybe it's not too restful
+  // Gets the id as the argument after joke
+  app.put("/joke/:id", (req, res) => {
+    // TODO:
+    // Would be nice to check the data given, matching it with a template format
+    var joke = req.body;
+    // Just check if present for now
+    if (typeof joke === "undefined" || joke.length < 1) {
+      res.status(200).send(`Expected a body like this: 
         {
           "id" : STRING, ID unique for each JOKE
           "icon_url" : STRING, URL of a valid web compatible image
@@ -154,50 +178,50 @@ const jokeRoutes = (app, fs) => {
           "categories": STRING[], the list of categories for the JOKE
           }
         `);
-        res.end();
-        return;
+      res.end();
+      return;
+    }
+    // read the data first
+    readFile((data) => {
+      // get the id
+      const jokeId = req.params["id"];
+      // Find the joke
+      const index = findJoke(data, jokeId);
+      if (index >= 0) {
+        // Keeping id as it was prior the update
+        joke.id = data[index].id;
+        // set the new data
+        data[index] = joke;
+        writeFile(JSON.stringify(data, null, 2), () => {
+          res.status(200).send(`joke (id:${jokeId}) is updated`);
+        });
+      } else {
+        res.status(200).send(`There is no joke with id:${jokeId}`);
       }
-      // read the data first
-      readFile(data => {
-        // get the id
-        const jokeId = req.params['id'];
-        // Find the joke
-        const index = findJoke(data,jokeId);
-        if(index >= 0) {
-          // set the new data
-          data[index] = req.body;
-          writeFile(JSON.stringify(data, null, 2), () => {
-            res.status(200).send(`joke (id:${jokeId}) is updated`);
-          });
-        }
-        else {
-          res.status(200).send(`There is no joke with id:${jokeId}`);
-        }
-      }, true);
-    });
+    }, true);
+  });
 
-    // DELETE
-    // Note the route is different from the routes on multiple jokes (is <joke> and not <jokes>), maybe it's not too restful
-    // Gets the id as the argument after joke
-    app.delete('/joke/:id', (req, res) => {
-      // read the data first
-      readFile(data => {
-        // get the id
-        const jokeId = req.params['id'];
+  // DELETE
+  // Note the route is different from the routes on multiple jokes (is <joke> and not <jokes>), maybe it's not too restful
+  // Gets the id as the argument after joke
+  app.delete("/joke/:id", (req, res) => {
+    // read the data first
+    readFile((data) => {
+      // get the id
+      const jokeId = req.params["id"];
 
-        // Find the joke
-        const index = findJoke(data,jokeId);
-        if(index >= 0) {
-          data.splice(index,1);
-          writeFile(JSON.stringify(data, null, 2), () => {
-            res.status(200).send(`joke (id:${jokeId}) is removed`);
-          });
-        }
-        else {
-          res.status(200).send(`There is no joke with id:${jokeId}`);
-        }
-      }, true);
-    });
-  };
-  
-  module.exports = jokeRoutes;
+      // Find the joke
+      const index = findJoke(data, jokeId);
+      if (index >= 0) {
+        data.splice(index, 1);
+        writeFile(JSON.stringify(data, null, 2), () => {
+          res.status(200).send(`joke (id:${jokeId}) is removed`);
+        });
+      } else {
+        res.status(200).send(`There is no joke with id:${jokeId}`);
+      }
+    }, true);
+  });
+};
+
+module.exports = jokeRoutes;
